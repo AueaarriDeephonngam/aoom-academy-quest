140e86202c7ad8f526d2d09170c7737fea5ec5d7
aueaarri.d <aueaarri.d@kkumail.com>
2025-08-20 09:58:43 +0700

    fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'quests#index'
-  
-  resources :quests, only: [:index, :create, :destroy] do
+  root "quests#index"
+
+  resources :quests, only: [ :index, :create, :destroy ] do
     member do
       patch :toggle
     end
   end
-  
 end
